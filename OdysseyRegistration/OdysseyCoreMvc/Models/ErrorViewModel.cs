f363c1daffb9c6d1aed6bd06dbdc634791a57a92
Rob Bernstein <rob@tardistech.com>
2022-08-03 20:51:34 -0400

    Added OdysseyCoreMvc project.

---
+++ OdysseyRegistration/OdysseyCoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OdysseyCoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
