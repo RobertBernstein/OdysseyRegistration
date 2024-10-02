bbc898aca0ec1dcf397b7c3812c03979ebbaff63
Rob Bernstein <rob@tardistech.com>
2024-10-01 22:19:50 -0400

    Restoring comments and more to HomeController.cs from original source code.

--- OdysseyRegistration/OdysseyMvc2023/Controllers/HomeController.cs
+++ OdysseyRegistration/OdysseyMvc2023/Controllers/HomeController.cs
@@ -1,4 +1,13 @@
-﻿// Decompiled with JetBrains decompiler
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HomeController.cs" company="Tardis Technologies">
+//   Copyright 2014 Tardis Technologies. All rights reserved.
+// </copyright>
+// <summary>
+//   The home controller.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+// Decompiled with JetBrains decompiler
 // Type: OdysseyMvc4.Controllers.HomeController
 // Assembly: OdysseyMvc4, Version=1.0.5460.36587, Culture=neutral, PublicKeyToken=null
 // MVID: 7B658547-521F-44CB-80FA-52857CB94B72
@@ -10,17 +19,35 @@ using OdysseyMvc2023.ViewData;
 
 namespace OdysseyMvc2023.Controllers
 {
-  public class HomeController : BaseRegistrationController
-  {
-    private readonly OdysseyRepository repository = new OdysseyRepository();
 
-    [HttpGet]
-    public ActionResult Index()
+    /// <summary>
+    /// The home controller.
+    /// </summary>
+    public class HomeController : BaseRegistrationController
     {
-      BaseViewData baseViewData = new BaseViewData();
-      this.SetBaseViewData(baseViewData);
-      this.ViewData["Message"] = (object) ("Welcome to the " + this.repository.Config["RegionName"] + " Odyssey of the Mind Region " + this.repository.Config["RegionNumber"] + " Registration web pages.");
-      return (ActionResult) this.View((object) baseViewData);
+        /// <summary>
+        /// Provides database access.
+        /// </summary>
+        private readonly OdysseyRepository repository = new OdysseyRepository();
+
+        /// <summary>
+        /// The view for the Index page.
+        /// GET: /Home/
+        /// </summary>
+        /// <returns>
+        /// The <see cref="ActionResult"/>.
+        /// </returns>
+        [HttpGet]
+        public ActionResult Index()
+        {
+            BaseViewData baseViewData = new BaseViewData();
+            this.SetBaseViewData(baseViewData);
+
+            this.ViewData["Message"] =
+                      (object)("Welcome to the " + this.repository.Config["RegionName"] + " Odyssey of the Mind Region " +
+                      this.repository.Config["RegionNumber"] + " Registration web pages.");
+
+            return (ActionResult)this.View((object)baseViewData);
+        }
     }
-  }
 }
