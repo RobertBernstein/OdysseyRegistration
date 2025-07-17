f33d205d68762cfa7dd8268449e65da5e5e940bb
Rob Bernstein <rob@tardistech.com>
2025-07-17 16:47:39 +0300

    Update SQL login script for enhanced security
    Changed the password for the 'vaodyssey' login from a hardcoded value to a variable placeholder. This improves security by eliminating the use of a static password in the script.

--- OdysseyRegistration/init/init.sql
+++ OdysseyRegistration/init/init.sql
@@ -3,7 +3,7 @@ GO
 
 IF NOT EXISTS (SELECT * FROM sys.sql_logins WHERE name = 'vaodyssey')
 BEGIN
-    CREATE LOGIN [vaodyssey] WITH PASSWORD = 'password123', CHECK_POLICY = OFF;
+    CREATE LOGIN [vaodyssey] WITH PASSWORD = '$(sa_password)', CHECK_POLICY = OFF;
     ALTER SERVER ROLE [sysadmin] ADD MEMBER [vaodyssey];
 END
 GO
